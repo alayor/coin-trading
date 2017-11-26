de41b5b49fd886b8286d770861670d764f00ffc2
alayor <alayor3@gmail.com>
2017-11-26 09:00:53 -0600

    Add mocked iTest to check trades retrieving with marker method.

--- src/integrationTests/mocked/java/mockedIntegrationTests/BitsoApiRequester_MockedIntegrationTest.java
+++ src/integrationTests/mocked/java/mockedIntegrationTests/BitsoApiRequester_MockedIntegrationTest.java
@@ -10,8 +10,7 @@ import service.tools.BitsoApiRequester;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class BitsoApiRequester_MockedIntegrationTest {
     private BitsoApiRequester bitsoApiRequester;
@@ -42,5 +41,27 @@ public class BitsoApiRequester_MockedIntegrationTest {
         assertEquals("buy", tradeResult.getTradeList().get(0).getMakerSide());
         assertEquals("163310.07", tradeResult.getTradeList().get(0).getPrice());
         assertEquals("321748", tradeResult.getTradeList().get(0).getTid());
+    }
+
+    @Test
+    public void shouldParseEmptyFailedTradeResult() throws URISyntaxException, IOException {
+        // given
+        bitsoApiRequester = new BitsoApiRequester("http://localhost:9999/singleFailedTradeFixture.json");
+        // when
+        TradeResult tradeResult = bitsoApiRequester.getTrades(3);
+        // then
+        assertFalse(tradeResult.isSuccess());
+        assertEquals(0, tradeResult.getTradeList().size());
+    }
+
+    @Test
+    public void shouldGetTradesSince() throws Exception {
+        // given
+        bitsoApiRequester = new BitsoApiRequester("http://localhost:9999/multipleTradesFixture.json");
+        // when
+        TradeResult tradeResult = bitsoApiRequester.getTradesSince(2128418);
+        // then
+        assertTrue(tradeResult.isSuccess());
+        assertEquals(25, tradeResult.getTradeList().size());
     }
 }
