6130ae67ec3bc6b4fbc4ded6cfaf3347becd9c07
alayor <alayor3@gmail.com>
2017-11-26 10:30:26 -0600

    Set setRemoveOnCancelPolicy = true to ScheduleExecutor.

--- src/main/java/service/TradingService.java
+++ src/main/java/service/TradingService.java
@@ -7,8 +7,8 @@ import service.tools.BitsoApiRequester;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
-import static java.util.concurrent.Executors.newScheduledThreadPool;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class TradingService {
@@ -18,8 +18,14 @@ public class TradingService {
     private final Runnable updateTradesRunnable = this::updateTrades;
     private ArrayBlockingQueue<Trade> trades = new ArrayBlockingQueue<>(2000);
 
+    private static ScheduledThreadPoolExecutor getScheduledThreadPoolExecutor() {
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(1);
+        scheduledThreadPoolExecutor.setRemoveOnCancelPolicy(true);
+        return scheduledThreadPoolExecutor;
+    }
+
     public TradingService(BitsoApiRequester bitsoApiRequester) {
-        this(bitsoApiRequester, newScheduledThreadPool(1));
+        this(bitsoApiRequester, getScheduledThreadPoolExecutor());
     }
 
     TradingService(BitsoApiRequester bitsoApiRequester, ScheduledExecutorService executor) {
